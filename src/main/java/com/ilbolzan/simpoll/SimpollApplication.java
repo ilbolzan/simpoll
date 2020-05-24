c5cb99c96b8048fea4803abf5f639f5db46b09df
Ílson Bolzan <ilson.bolzan@zenvia.com>
2020-05-24 18:06:44 -0300

    Project started

---
+++ src/main/java/com/ilbolzan/simpoll/SimpollApplication.java
@@ -0,0 +1,13 @@
+package com.ilbolzan.simpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilbolzan/simpoll/SimpollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilbolzan.simpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
